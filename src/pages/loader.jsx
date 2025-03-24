45f86aaec477cfe46ebf7a9ccbf0bac91fe286c4
github_DeboAdeniran <raqibadeniran03@gmail.com>
2025-03-24 13:57:20 +0100

    fixed the signup

---
+++ src/pages/loader.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Loader() {
+  return (
+    <div className="loader_overlay">
+      <span className="loader"></span>
+    </div>
+  );
+}
+export default Loader;
